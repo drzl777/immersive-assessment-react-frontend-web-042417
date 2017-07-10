7f7a8b16204a222c79834a45cede8e92f7b4ea61
drzl777 <andrew.zheng725@gmail.com>
2017-07-10 10:00:59 -0400

    Done.

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,7 +4,7 @@ import Search from './Search'
 
 class AccountContainer extends Component {
 
-  constructor() {
+  constructor () {
     super()
 
     // we have provided this default state for you,
@@ -13,48 +13,25 @@ class AccountContainer extends Component {
 
     this.state = {
       searchTerm: '',
-      transactions: [
-        {
-          id: 1,
-          posted_at: "2017-02-28 11:00:00",
-          description: "Leather Pants, Gap co.",
-          category: "Fashion",
-          amount: -20000
-        },
-        {
-          id: 2,
-          posted_at: "2017-02-29 10:30:00",
-          description: "Paycheck from Bob's Burgers",
-          category: "Income",
-          amount: 100000
-        },
-        {
-          id: 3,
-          posted_at: "2017-05-24 10:53:00",
-          description: "'Pair Programming Illuminated' by Laurie Williams and Robert Kessler",
-          category: "Book",
-          amount: 1498
-        },
-        {
-          id: 4,
-          posted_at: "2017-05-24 08:52:00",
-          description: "Medium Iced Cold Brew, Gregory's Coffee",
-          category: "Coffee",
-          amount: 365
-        }
-      ]
+      transactions: []
     }
   }
 
-  handleChange(event) {
-    // your code here
+  componentWillMount () {
+    fetch('https://boiling-brook-94902.herokuapp.com/transactions')
+      .then(resp => resp.json())
+      .then(transactions => this.setState({transactions}))
   }
 
-  render() {
+  handleChange (event) {
+    const searchTerm = event.target.value
+    this.setState({searchTerm})
+  }
 
+  render () {
     return (
       <div>
-        <Search searchTerm={this.state.searchTerm} handleChange={"...add code here..."} />
+        <Search searchTerm={this.state.searchTerm} handleChange={this.handleChange.bind(this)} />
         <TransactionsList transactions={this.state.transactions} searchTerm={this.state.searchTerm} />
       </div>
     )
